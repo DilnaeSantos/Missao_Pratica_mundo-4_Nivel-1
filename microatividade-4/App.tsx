b2970d7b02f92635e8b89797777da0e2918c3ba8
DilnaeSantos <dilnaerennan84@gmail.com>
2024-09-16 16:45:50 -0300

    microatividade4 concluida

--- microatividade-4/App.tsx
+++ microatividade-4/App.tsx
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React from 'react';
+
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+
+ 
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 22,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
   },
+
+  item: {
+    padding: 10,
+
+    fontSize: 18,
+
+    height: 44,
+
+  },
+
 });
+
+ 
+
+const FlatListBasics = () => {
+
+  return (
+
+    <View style={styles.container}>
+
+      <FlatList
+
+        data={[
+
+          {key: 'Devin'},
+
+          {key: 'Dan'},
+
+          {key: 'Dominic'},
+
+          {key: 'Jackson'},
+
+          {key: 'James'},
+
+          {key: 'Joel'},
+
+          {key: 'John'},
+
+          {key: 'Jillian'},
+
+          {key: 'Jimmy'},
+
+          {key: 'Julie'},
+
+        ]}
+
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+
+      />
+
+    </View>
+
+  );
+
+};
+
+ 
+
+export default FlatListBasics;
