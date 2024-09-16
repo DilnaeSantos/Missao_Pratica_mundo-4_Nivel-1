8d9c6279f5fa675856bdc84031e69e82e53aef30
DilnaeSantos <dilnaerennan84@gmail.com>
2024-09-16 19:49:13 -0300

    microatividade-5 concluida

--- microatividade-5/App.tsx
+++ microatividade-5/App.tsx
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import {View, Image, StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
+
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    flex: 1,
   },
+
+  tinyLogo: {
+    width: 60,
+    height: 50,
+    marginBottom: 20,
+  },
+
+  Logo: {
+    width: 200,
+    height: 90,
+    marginBottom: 20,
+  }
+
 });
+ 
+
+const DisplayAnImage = () => {
+
+  return (
+
+    <View style={styles.container}>
+
+      <Image
+        style={styles.Logo}
+        source={require('./assets/logo2.jpg')}
+      />
+
+      <Image
+
+        style={styles.tinyLogo}
+
+        source={{
+
+          uri: 'https://reactnative.dev/img/tiny_logo.png',
+
+        }}
+
+      />
+
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
+        }}
+      />
+
+    </View>
+
+  );
+
+};
+
+ 
+
+export default DisplayAnImage;
