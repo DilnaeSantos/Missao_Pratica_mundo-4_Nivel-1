d8bbf00d1c1d2b6a316d5c81e9f9ffd2547270c2
DilnaeSantos <dilnaerennan84@gmail.com>
2024-09-18 14:40:06 -0300

    microatividade-5_parte-2 concluida

--- microatividade-5_parte-2/App.tsx
+++ microatividade-5_parte-2/App.tsx
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import {View, Image, StyleSheet} from 'react-native';
+
+ 
 
 const styles = StyleSheet.create({
+
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+    paddingTop: 50,
+
   },
+
+  stretch: {
+
+    width: 100,
+
+    height: 200,
+
+    resizeMode: 'stretch',
+
+  },
+
 });
+
+ 
+
+const DisplayAnImageWithStyle = () => {
+
+  return (
+
+    <View style={styles.container}>
+
+      <Image
+
+        style={styles.stretch}
+
+        source={require('./assets/logo2.jpg')}
+
+      />
+
+    </View>
+
+  );
+
+};
+
+export default DisplayAnImageWithStyle;
